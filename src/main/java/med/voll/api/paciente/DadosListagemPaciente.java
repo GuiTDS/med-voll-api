c5d1383eb9f01bf53e71028cf97458e4e0f4dc4c
Guilherme Sanches <guitdsanches@gmail.com>
2023-06-05 11:29:40 -0300

    adding get method for patients

---
+++ src/main/java/med/voll/api/paciente/DadosListagemPaciente.java
@@ -0,0 +1,7 @@
+package med.voll.api.paciente;
+
+public record DadosListagemPaciente(Long id, String nome, String email, String cpf) {
+    public DadosListagemPaciente(Paciente dados) {
+        this(dados.getId(), dados.getNome(), dados.getEmail(), dados.getCpf());
+    }
+}
